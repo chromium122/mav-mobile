c0fc73ef338e512a1064202da190dd06f87d47f0
chromium122 <chromium@airmail.cc>
2023-06-01 16:37:06 +0200

    initial commit

---
+++ bin/mav_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:mav_mobile/mav_mobile.dart' as mav_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mav_mobile.calculate()}!');
+}
---
+++ test/mav_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:mav_mobile/mav_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
